2e4b4e97274fe8a7a0d2e950dd5a7b15d0046af2
Eric Shumway <ericshumway2@mail.weber.edu>
2022-04-28 21:00:23 -0600

    Adding intial set up of controllers for comments, expenses, income, and user, as well as homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const { User,  Income, Expense } = require('../models');
+
+//Get all info for homepage if user is logged in
+router.get('/user', async (req, res) => {
+    try {
+      //redirect to login page if user is not logged in
+      console.log(req.session.logged_in);
+      if (!req.session.logged_in) {
+        res.redirect('/');
+        return;
+      }
+  
+      // Get all posts
+      const incomeData = await Income.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['id'],
+            where: {
+              id: req.session.user_id, //only get incomes that match user_id
+            },
+          },
+        ],
+      });
+      console.log(incomeData);
+
+      // Get all expenses
+      const expenseData = await Expense.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['id'],
+            where: {
+              id: req.session.user_id, //only get expenses that match user_id
+            },
+          },
+        ],
+      });
+  
+      //Get user data
+      const userData = await User.findByPk(req.session.user_id);
+      console.log('THE USER DATA: ' + userData);
+      console.log(typeof userData);
+  
+      // Serialize data so the template can read it
+      const incomes = incomeData.map((income) => income.get({ plain: true }));
+  
+      const expenses = expenseData.map((expense) =>
+        expense.get({ plain: true })
+      );
+  
+      //const user = userData.map((user) => user.get({ plain: true }));
+      const user = userData.get({ plain: true });
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', {
+        incomes,
+        expenses,
+        user,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
