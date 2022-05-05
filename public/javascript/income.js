184868bc0b8ec800b6237c9ce5fb23e2441f66fd
= <holdanmcmullen@gmail.com>
2022-05-05 12:48:18 -0600

    fixed incomes delete function

--- public/javascript/income.js
+++ public/javascript/income.js
@@ -60,4 +60,4 @@ async function deleteIncome(event) {
 
 document.querySelector(".addIncomeForm").addEventListener("submit", addIncome);
 
-document.querySelector('#expenses-list').addEventListener('click', deleteIncome);
+document.querySelector('#incomes-list').addEventListener('click', deleteIncome);
