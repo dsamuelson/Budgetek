7ca80473602f0e3034743fda4846a42fadcc3d6f
Benny623 <bmilner88@gmail.com>
2022-05-05 10:37:30 -0600

    fixed addExpense.js and addIncome.js

--- public/javascript/addExpense.js
+++ public/javascript/addExpense.js
@@ -1,47 +1,47 @@
 async function addExpense(event) {
-        event.preventDefault();
-      
-        const title = document.querySelector("#expense-name").value.trim();
-        const cost = document.querySelector("#expense-cost").value.trim();
-        const frequency = document.querySelector("#expense-frequency").value.trim();
-        const user_id = document.querySelector('#user-id').textContent;
-        const ele = document.getElementsByName("priority");
-        let priority;
-        let is_vital;
+  event.preventDefault();
 
-        for(i = 0; i < ele.length; i++) {
-            if(ele[i].checked) {
-                priority = ele[i].value;
-            }
-        }
+  const title = document.querySelector("#expense-name").value.trim();
+  const cost = document.querySelector("#expense-cost").value.trim();
+  const frequency = document.querySelector("#expense-frequency").value.trim();
+  const user_id = document.querySelector("#user-id").textContent;
+  const ele = document.getElementsByName("priority");
+  let priority;
+  let is_vital;
 
-        if(priority === 'Primary') {
-            is_vital = true;
-        } else {
-            is_vital = false;
-        }
-      
-        if (title && cost && frequency && priority) {
-          const response = await fetch("/api/expense", {
-            method: "POST",
-            body: JSON.stringify({
-              title,
-              cost,
-              frequency,
-              is_vital,
-              user_id,
-            }),
-            headers: { "Content-Type": "application/json" },
-          });
-      
-          if (response.ok) {
-            sessionStorage.setItem('currentTab', 'Expenses');
-            document.location.replace("/user");
-          } else {
-            alert(response.statusText);
-          }
-        }
-};
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      priority = ele[i].value;
+    }
+  }
+
+  if (priority === "vital") {
+    is_vital = true;
+  } else {
+    is_vital = false;
+  }
+
+  if (title && cost && frequency && priority) {
+    const response = await fetch("/api/expense", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        cost,
+        frequency,
+        is_vital,
+        user_id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      sessionStorage.setItem("currentTab", "Expenses");
+      document.location.replace("/user");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
 
 document
   .querySelector(".addExpenseForm")
--- public/javascript/addIncome.js
+++ public/javascript/addIncome.js
@@ -1,48 +1,46 @@
 async function addIncome(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector("#income-name").value.trim();
-    const pay = document.querySelector("#income-pay").value.trim();
-    const frequency = document.querySelector("#income-frequency").value.trim();
-    const user_id = document.querySelector('#user-id').textContent;
-    const ele = document.getElementsByName("priority");
-    let priority;
-    let is_primary;
+  event.preventDefault();
 
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked) {
-            priority = ele[i].value;
-        }
+  const title = document.querySelector("#income-name").value.trim();
+  const pay = document.querySelector("#income-pay").value.trim();
+  const frequency = document.querySelector("#income-frequency").value.trim();
+  const user_id = document.querySelector("#user-id").textContent;
+  const ele = document.getElementsByName("priority");
+  let priority;
+  let is_primary;
+
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      priority = ele[i].value;
     }
+  }
 
-    if(priority === 'Primary') {
-        is_primary = true;
+  if (priority === "Primary") {
+    is_primary = true;
+  } else {
+    is_primary = false;
+  }
+
+  if (title && pay && frequency && priority) {
+    const response = await fetch("/api/income", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        pay,
+        frequency,
+        is_primary,
+        user_id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      sessionStorage.setItem("currentTab", "Income");
+      document.location.replace("/user");
     } else {
-        is_primary = false;
+      alert(response.statusText);
     }
-  
-    if (title && pay && frequency && priority) {
-      const response = await fetch("/api/income", {
-        method: "POST",
-        body: JSON.stringify({
-          title,
-          pay,
-          frequency,
-          is_primary,
-          user_id,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-  
-      if (response.ok) {
-        sessionStorage.setItem('currentTab', 'Income');
-        document.location.replace("/user");
-      } else {
-        alert(response.statusText);
-      }
-    }
-};
+  }
+}
 
-document
-.querySelector(".addIncomeForm")
-.addEventListener("submit", addIncome);
+document.querySelector(".addIncomeForm").addEventListener("submit", addIncome);
